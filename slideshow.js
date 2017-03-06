62cc69579dc5415885e222fd480027dd83320e82
Viljami <viljami.virolainen@aalto.fi>
2017-03-06 15:24:16 +0200

    päivitett json haku toimivaks

--- slideshow.js
+++ slideshow.js
@@ -1,3 +1,3 @@
 window.onload = function(event) {
-	$.getJSON("https://virolav2-e203f.firebaseio.com/.json").done(console.log(data))	
+	jQuery.getJSON("https://virolav2-e203f.firebaseio.com/.json").done(console.log(data))	
 }
