8924212150d95168632df83be932c8a039a7b640
Viljami <viljami.virolainen@aalto.fi>
2017-03-20 15:42:49 +0200

    nyt pitäs olla ok

--- slideshow.js
+++ slideshow.js
@@ -1,7 +1,13 @@
 window.onload = function(event) {
 	$.getJSON("https://virolav2-e203f.firebaseio.com/.json", function( data ) {
 			for (var i = data.articles.length - 1; i >= 0; i--) {
-				$( "<li class =\"article\">").appendTo("#articles");
+				if(i===0) {
+					$( "<li class =\"article showing"+ "\">").appendTo("#articles");
+				}
+				else {
+					$( "<li class =\"article"+ "\">").appendTo("#articles");	
+				}
+				
 				$( "<p>"+ data.articles[i].sisältö +"</p>" ).appendTo( "#articles" );
 				$( "<img src="+data.articles[i].kuva+">").appendTo("#articles");
 				$( "</li>").appendTo("#articles");
@@ -9,6 +15,16 @@ window.onload = function(event) {
 			
 			console.log(data);
 	});
-		
-	
 }
+
+var articles = document.querySelectorAll('#articles .article');
+var currentArticle = 0;
+var articleInterval = setInterval(nextArticle,2000);
+
+function nextArticle() {
+    articles[currentArticle].className = 'article';
+    currentArticle = (currentArticle+1)%articles.length;
+    articles[currentArticle].className = 'article showing';
+}
+
+
